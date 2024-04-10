d4c5ec787955651a1035d3c820fd77c810420c91
ytawfiq20 <ymtawfiq2003@gmail.com>
2024-04-10 11:41:39 +0200

    Convert to asynchronous

--- Ecommerce.Client/obj/Debug/net8.0/Ecommerce.Client.AssemblyInfo.cs
+++ Ecommerce.Client/obj/Debug/net8.0/Ecommerce.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d406e7aad44ebfc5b16a9ea5e26e1f339b04336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+560cb51ab2f19c2edd9648bc38966ea4a49fde48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
